4904eb63db71090a01e6bfc20aba30803d69ce61
Bilawal <bilawalriaz@gmail.com>
2024-10-17 11:08:56 +0100

    jhjk

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,15 +2,15 @@ import { defineConfig } from "drizzle-kit";
 import { loadEnvConfig } from "@next/env";
 
 // Load environment variables
-const dev = process.env.NODE_ENV !== "production";
-loadEnvConfig("./", dev);
+//const dev = process.env.NODE_ENV !== "production";
+//loadEnvConfig("./", dev);
 
 export default defineConfig({
   schema: "lib/db/schema.ts",
   out: "lib/db/drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.POSTGRES_URL!,
+    url: 'postgres://default:L3gfFzdU4KyB@ep-wandering-surf-99229463.eu-central-1.aws.neon.tech:5432/verceldb?sslmode=require',
   },
   verbose: true,
   strict: true,
